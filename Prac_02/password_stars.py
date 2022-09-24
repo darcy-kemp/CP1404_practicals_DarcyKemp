6dc9fee1113f6734455738cf79d1f5479b90cc6c
darcy-kemp <darcy.kemp@my.jcu.edu.au>
2022-09-24 20:03:52 +1000

    Refactored password check program to use functions

---
+++ Prac_02/password_stars.py
@@ -0,0 +1,21 @@
+MIN_PW_LENGTH = 6
+
+
+def main():
+    user_pw = get_password()
+
+    print_stars(user_pw)
+
+
+def print_stars(password):
+    print('*' * len(password))
+
+
+def get_password():
+    user_pw = input("please input a password: ")
+    while len(user_pw) < MIN_PW_LENGTH:
+        user_pw = input("please enter a valid password: ")
+    return user_pw
+
+
+main()
