88a3dc77c4a8646a36f341f0ba9fe0489b22705e
darcy-kemp <darcy.kemp@my.jcu.edu.au>
2022-10-12 16:12:00 +1000

    Prac 4 quick picks commit.

---
+++ Prac_04/quick_picks.py
@@ -0,0 +1,26 @@
+"""CP1404 | prac_04 list comprehension | Darcy Kemp
+User selects how many "picks" then multiple random number lines are returned
+"""
+import random
+LOWER_RANDOM_BOUNDARY = 1
+UPPER_RANDOM_BOUNDARY = 45
+
+
+def main():
+
+    number_of_picks = int(input("How many picks?: "))
+
+    for i in range(1, number_of_picks + 1, 1):
+        random_lottery_pick = [0, 0, 0, 0, 0, 0]
+
+        for j in range(6):
+            current_random_int = random.randint(LOWER_RANDOM_BOUNDARY,UPPER_RANDOM_BOUNDARY)
+
+            while current_random_int in random_lottery_pick:
+                current_random_int = random.randint(LOWER_RANDOM_BOUNDARY, UPPER_RANDOM_BOUNDARY)
+
+            random_lottery_pick[j] = current_random_int
+
+        print(f"Pick number {i} is: {random_lottery_pick}")
+
+main()
