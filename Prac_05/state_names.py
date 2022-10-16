18b93fdaba77a9d44afa58a04b28027ae461875e
darcy-kemp <darcy.kemp@my.jcu.edu.au>
2022-10-16 23:36:23 +1000

    Prac 5 completed hex colours exercise and removed redundant variables from state names.

--- Prac_05/state_names.py
+++ Prac_05/state_names.py
@@ -5,8 +5,7 @@ State names in a dictionary
 # Reformat this file so the dictionary code follows PEP 8 convention
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
-codes = CODE_TO_NAME.keys()
-names = CODE_TO_NAME.values()
+
 
 for key in CODE_TO_NAME:
     print(f"{key:3} is {CODE_TO_NAME[key]}")
