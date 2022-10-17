a72550ae6612d7e699fe2528ac07e900f6e25fa7
darcy-kemp <darcy.kemp@my.jcu.edu.au>
2022-10-17 16:30:38 +1000

    Prac 5 completed word occurrences working with sorting and formats to length of longest word.

--- Prac_05/word_occurances.py
+++ Prac_05/word_occurances.py
@@ -4,9 +4,16 @@ program to count occurrences of a word in a string
 """
 user_input_string = str(input("Text: "))
 word_to_occurrences = {}
+max_word_length = 0
 
 for word in user_input_string.split(" "):
     word_to_occurrences[word] = word_to_occurrences.get(word, 0) + 1
 
+    if len(word) > max_word_length:
+        max_word_length = len(word)
+
+word_to_occurrences = dict(sorted(word_to_occurrences.items()))
+
+
 for key in word_to_occurrences:
-    print(f"{key} is {word_to_occurrences[key]}")
+    print(f"{key:{max_word_length}} is {word_to_occurrences[key]}")
