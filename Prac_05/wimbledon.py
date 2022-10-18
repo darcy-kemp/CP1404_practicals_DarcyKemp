c34978be2dc04a7b46aec231ff9e39eebc326dfb
darcy-kemp <darcy.kemp@my.jcu.edu.au>
2022-10-18 16:04:20 +1000

    Prac 5 changed wimbledon to use 4 functions and a set.

--- Prac_05/wimbledon.py
+++ Prac_05/wimbledon.py
@@ -4,23 +4,45 @@ est:30
 real:30
 """
 FILE_NAME = "wimbledon.csv"
-champion_to_number_of_wins = {}
-countries_that_won_wimbledon = []
+CHAMPION_INDEX = 2
+COUNTRY_INDEX = 1
 
-with open(FILE_NAME, "r", encoding="utf-8-sig") as in_file:
-    next(in_file)
-    for line in in_file:
-        current_data_line = line.split(",")
-        champion_to_number_of_wins[current_data_line[2]] = champion_to_number_of_wins.get(current_data_line[2], 0) + 1
-        country = current_data_line[1]
 
-        if country not in countries_that_won_wimbledon:
-            countries_that_won_wimbledon.append(country)
+def read_file(name_of_file):
+    with open(name_of_file, "r", encoding="utf-8-sig") as in_file:
+        wimbledon_data = []
+        next(in_file)
+        for line in in_file:
+            wimbledon_data.append(line.split(","))
+    return wimbledon_data
 
-print("Wimbledon champions:")
-for key in champion_to_number_of_wins:
-    print(f"{key} is {champion_to_number_of_wins[key]}")
 
-print(f"\nThese {len(countries_that_won_wimbledon)} countries have won wimbledon:")
-countries_that_won_wimbledon = ", ".join(countries_that_won_wimbledon)
-print(f"{countries_that_won_wimbledon}")
+def process_data(data, country_index, champion_index):
+    countries = set()
+    champion_to_victories = {}
+    for entry in data:
+        champion_to_victories[entry[champion_index]] = champion_to_victories.get(entry[champion_index], 0) + 1
+        countries.add(entry[country_index])
+    return champion_to_victories, countries
+
+
+def print_processed_data(champion_to_victories, countries):
+    print("Wimbledon champions:")
+    for key in champion_to_victories:
+        print(f"{key} won {champion_to_victories[key]} times.")
+
+    print(f"\nThese {len(countries)} countries have won wimbledon:")
+    countries = ", ".join(sorted(countries))
+    print(f"{countries}")
+
+
+def main(file_name, country_index, champion_index):
+
+    wimbledon_data = read_file(file_name)
+
+    champion_to_number_of_wins, countries_won_wimbledon = process_data(wimbledon_data, country_index, champion_index)
+
+    print_processed_data(champion_to_number_of_wins, countries_won_wimbledon)
+
+
+main(FILE_NAME, COUNTRY_INDEX, CHAMPION_INDEX)
