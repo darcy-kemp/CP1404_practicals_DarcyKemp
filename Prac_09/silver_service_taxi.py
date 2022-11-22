222c285d041ac2a62c62652bdfe7d942be628272
darcy-kemp <darcy.kemp@my.jcu.edu.au>
2022-11-22 15:59:20 +1000

    Fix silver service class multiplying distance by fanciness squared.

--- Prac_09/silver_service_taxi.py
+++ Prac_09/silver_service_taxi.py
@@ -3,22 +3,21 @@ from Prac_09.taxi import Taxi
 
 class SilverServiceTaxi(Taxi):
 
-    def __init(self, name, fuel, fanciness):
+    def __init__(self, name, fuel, fanciness):
         flagfall = 4.50
         super().__init__(name, fuel)
         self.fanciness = fanciness
-        self.price_per_km *= fanciness
+        self.price_per_km = fanciness * self.price_per_km
         self.flagfall = flagfall
 
     def get_fare(self):
         """Return the price for the trip."""
-        return super().get_fare() * self.fanciness + self.flagfall
+        return super().get_fare() + self.flagfall
 
     def drive(self, distance):
         distance_driven = super().drive(distance)
-        self.current_fare_distance += distance_driven
         return distance_driven
 
     def __str__(self):
-        return f"{self.name}, fuel = {self.fuel}, odometer = {self._odometer}, {self.current_fare_distance} on " \
-               f"current fare, {(self.fanciness * self.price_per_km):.2f}/km plus flagfall of {self.flagfall} "
+        return f"{self.name}, fuel = {self.fuel}, odometer = {self._odometer}, {self.current_fare_distance}km on " \
+               f"current fare, ${(self.fanciness * self.price_per_km):.2f}/km plus flagfall of ${self.flagfall:.2f} "
---
+++ Prac_09/silver_service_taxi_test.py
@@ -0,0 +1,8 @@
+from Prac_09.silver_service_taxi import SilverServiceTaxi
+
+taxi1 = SilverServiceTaxi("Hummer", 200, 2)
+
+taxi1.drive(18)
+print(taxi1.price_per_km)
+print(taxi1)
+print(taxi1.get_fare())
