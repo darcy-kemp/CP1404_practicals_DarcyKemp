3a4233eb1d931dc086fde15db32ad40ac294c924
darcy-kemp <darcy.kemp@my.jcu.edu.au>
2022-11-23 22:01:22 +1000

    Complete taxi simulator task.

--- Prac_09/taxi_simulator.py
+++ Prac_09/taxi_simulator.py
@@ -2,19 +2,51 @@
 Program to simulate the use of taxis for a user."""
 from Prac_09.taxi import Taxi
 from Prac_09.silver_service_taxi import SilverServiceTaxi
+
 MENU = "q)uit, c)hoose taxi, d)rive"
 
 
 def main():
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
     current_taxi = None
+    print("Let's Drive!")
     print(MENU)
     user_menu_choice = input(">>>").lower()
-    while user_menu_choice == 'q':
+    current_cost = 0
+    while user_menu_choice != 'q':
+
         if user_menu_choice == 'c':
-            print('a')
+            # Error handling for index error. Did not include Value error as it was not shown in the sample.
+            display_taxis(taxis)
+            try:
+                current_taxi = taxis[int(input("Choose taxi: "))]
+            except IndexError:
+                print("Invalid taxi choice")
+            print(f"Bill to date: ${current_cost:.2f}")
+            print(MENU)
+            user_menu_choice = input(">>>").lower()
+
         elif user_menu_choice == 'd':
-            print('a')
+            if current_taxi is None:
+                print("You need to choose a taxi before you can drive.")
+            else:
+                current_taxi.start_fare()
+                current_taxi.drive(int(input("Drive how far? ")))
+                print(f"Your {current_taxi.name} trip cost you ${current_taxi.get_fare():.2f}")
+                current_cost += current_taxi.get_fare()
+            print(f"Bill to date: ${current_cost:.2f}")
+            print(MENU)
+            user_menu_choice = input(">>>").lower()
         else:
-            print('invalid menu choice')
-            user_menu_choice = input('>>>')
+            print('Invalid option')
+            user_menu_choice = input('>>>').lower()
+    print(f"Total trip cost: ${current_cost:.2f}")
+    display_taxis(taxis)
+
+
+def display_taxis(taxis):
+    for i, taxi in enumerate(taxis):
+        print(f"{i} - {taxi}")
+
+
+main()
